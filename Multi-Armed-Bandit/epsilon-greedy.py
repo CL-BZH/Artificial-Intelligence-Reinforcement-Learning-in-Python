728bbe836dca06374c6c0c08ab44b7d9f6038976
Christophe Louargant <christophe_louargant@hotmail.com>
2022-12-28 19:33:58 +0100

    Add Gaussian reward for Thompson sampling

--- Multi-Armed-Bandit/epsilon-greedy.py
+++ Multi-Armed-Bandit/epsilon-greedy.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import Callable
+#from typing import Callable
 from typing import List
 from typing import Optional
+#from abc import ABC
 
-from scipy.stats import beta
+from scipy import stats
+
 
 NUM_TRIALS = 10000
 EPSILON = 0.15
 Bernoulli_p = [0.2, 0.5, 0.75]
-Gaussian_params = [(5,2), (6,2), (1,5)]
+Gaussian_params = [(1,1), (2,1), (3,1)]
 
 class Epsilon:
     def __init__(self, epsilon: float):
@@ -57,7 +59,15 @@ class LinDecayEpsilon(Epsilon):
 
            
 class Distribution:
-    pass
+    
+    def __init__(self, name: str = None):
+        self.__name = name
+        if self.__name == None:
+            raise NotImplementedError("Distribution must have a name")
+    
+    @property
+    def name(self):
+        return self.__name
 
 
 class Bandit:
@@ -67,7 +77,7 @@ class Bandit:
         self.__N: int = 0 # Number of time this bandit was selected
         self.__e: float = 0 # Estimate of E
         # Define the type of a bandit as the distribution used for the reward
-        self.bandit_type = R.__class__.__name__
+        self.type = R.__class__.__name__
 
     @property
     def N(self) -> int:
@@ -96,6 +106,7 @@ class Bernoulli(Distribution):
         """
         p: Bernoulli parameter
         """
+        super().__init__("Bernoulli")
         self.p = p
         self.E = p
 
@@ -109,22 +120,53 @@ class Gaussian(Distribution):
         mean: The mean of the Gaussian distribution
         stdev: The standard deviation of the Gaussian distribution
         """
-        self.mean = mean
-        self.stdev = stdev
-        self.E = mean
-        
+        super().__init__("Gaussian")
+        self.__mean = mean
+        self.__stdev = stdev
+        self.__tau = 1 / (self.__stdev**2)
+        self.__E = mean
+        self.__lambda_: float = 1.
+    
+    @property
+    def E(self) -> float:
+        return self.__mean
+    
+    @property
+    def stdev(self) -> float:
+        return self.__stdev
+    
+    @property
+    def tau(self) -> float:
+        return self.__tau
+    
+    @property
+    def lambda_(self) -> float:
+        return self.__lambda_
+    
+    def pdf(self, x: float):
+        return stats.norm.pdf(x, self.E, self.stdev)
+    
+    def __add__(self, x ):
+        self.__mean = (self.tau * x + self.lambda_ * self.E) / (self.tau + self.lambda_)
+        self.__lambda_ += self.tau
+        self.__stdev = 1 / np.sqrt(self.lambda_)
+        self.__E = self.__mean
+        return self
+    
     def __call__(self) -> float:
-        return np.random.normal(self.mean, self.stdev)
+        return np.random.normal(self.__mean, self.__stdev)
 
 class Beta(Distribution):
     """
     See https://www.statlect.com/probability-distributions/beta-distribution
     """
     def __init__(self, alpha: float = 1., beta: float = 1.):
+        super().__init__("Beta")
         self.__alpha = alpha
         self.__beta = beta
         self.__E: float = alpha / (alpha + beta)
-
+        self.__stdev = np.sqrt((alpha * beta) / ((alpha + beta + 1)*((alpha+beta)**2)))
+                               
     @property
     def alpha(self) -> float:
         return self.__alpha
@@ -144,9 +186,22 @@ class Beta(Distribution):
     @property
     def E(self) -> float:
         return self.__alpha / (self.__alpha + self.__beta)
-        
+    
+    @property
+    def stdev(self) -> float:
+        return np.sqrt((self.alpha * self.beta) / ((self.alpha + self.beta + 1)*((self.alpha+self.beta)**2)))
+    
+    def pdf(self, x: float):
+        return stats.beta.pdf(x, self.__alpha, self.__beta)
+    
+    def __add__(self, R ):
+        self.__alpha += R
+        self.__beta += 1 - R
+        return self
+    
     def __call__(self) -> float:
         return np.random.beta(self.alpha, self.beta)
+
 
 
 class BanditAlgorithm:
@@ -232,7 +287,7 @@ class UCB(EpsilonGreedy):
     
     def exploit(self) -> int:
         if any(self.bandits_N == 0):
-            chosen = np.random.choice(self.bandits_indices[self.bandits_indices == 0])
+            chosen = np.random.choice(self.bandits_indices)
         else:
             t = self.bandits_N.sum()
             uncertainty = np.sqrt(self.c * np.log(t) / self.bandits_N)
@@ -249,31 +304,50 @@ class TS(BanditAlgorithm):
     Thompson Sampling algorithm for bandit selection.
     The implementation is done only for a Bernoulli likelihood.
     """
-    def __init__(self, bandits: List[Bandit]):
+    def __init__(self, bandits: List[Bandit], distribution: Distribution):
         super().__init__(bandits)
-        self.__Beta = np.array([[1, 1] for _ in range(self.bandits_count)])
+        self.__distribution = np.array([distribution() for _ in range(self.bandits_count)])
+        if distribution == Beta:
+            if any( [bandit.type != "Bernoulli" for bandit in self.bandits]):
+                raise ValueError(f"Wrong conjugates {[distribution.name for distribution in self.__distribution]} for"
+                                 f" bandits of type {[bandit.type for bandit in self.bandits]}")
+        elif distribution == Gaussian:
+            if any( [bandit.type != "Gaussian" for bandit in self.bandits]):
+                raise ValueError(f"Wrong conjugates {[distribution.name for distribution in self.__distribution]} for"
+                                 f" bandits of type {[bandit.type for bandit in self.bandits]}")
+        else:
+            raise ValueError(f"Wrong conjugates {[distribution.name for distribution in self.__distribution]} for"
+                                 f" bandits of type {[bandit.type for bandit in self.bandits]}")
             
+    @property
+    def distribution(self):
+        return self.__distribution
+    
     def exploit(self) -> int:
-        alpha, beta = self.__Beta[:,0], self.__Beta[:,1]
-        self.theta_samples = np.random.beta(alpha, beta)
-        chosen = np.argmax(self.theta_samples)
+        if any(self.bandits_N < 5):
+            # Baysian stats suppose that we have a good enough prior
+            chosen = np.random.choice(self.bandits_indices)
+        else:
+            theta_samples = [D() for D in self.distribution]
+            chosen = np.argmax(theta_samples)
         return chosen
 
     def update(self, R) -> None:
-        self.bandits_N[self.chosen] += 1 # This is just for information
-        self.__Beta[self.chosen] += (R, 1-R)
-
-    @property
-    def Beta(self):
-        return self.__Beta
+        self.bandits_N[self.chosen] += 1
+        self.distribution[self.chosen] += R
 
     def plot(self):
-        x = np.linspace(0, 1, 200)
-        for i, (alpha_, beta_) in enumerate(self.Beta):
-            y = beta.pdf(x, alpha_, beta_)
-            plt.plot(x, y, label=f"real p: {bandits[i].expected_reward():.2f}, " 
-                     f"win rate = {alpha_ - 1}/{bandits[i].N} = "
-                     f"{(alpha_ - 1) / bandits[i].N: .3f}")
+        argmin = np.argmin([D.E for D in self.distribution])
+        x_min = self.__distribution[argmin].E - 3 * self.__distribution[argmin].stdev
+        argmax = np.argmax([D.E for D in self.distribution])
+        x_max = self.__distribution[argmax].E + 4 * self.__distribution[argmax].stdev
+        x = np.linspace(x_min, x_max, 200)
+        for i, D in enumerate(self.distribution):
+            y = D.pdf(x)
+            plt.plot(x, y, label=f"\u03B8: {self.bandits[i].expected_reward():.2f}, " 
+                     #f"win rate = {D.alpha - 1}/{self.bandits[i].N} = "
+                     #f"{(D.alpha - 1) / self.bandits[i].N: .3f}. "
+                     f"\u03B8\u0302 = {D.E:.3f}")
         plt.title(f"Bandit distributions after {NUM_TRIALS} trials")
         plt.legend()
         plt.show()
@@ -310,7 +384,7 @@ def plot(test: Test, bandits: List[Bandit]):
     cumulative_gains = np.cumsum(test.gains)
     win_rates = cumulative_gains / (np.arange(test.trials) + 1)
     
-    distribution = bandits[0].bandit_type
+    distribution = bandits[0].type
     max = np.max([bandit.expected_reward() for bandit in bandits])
     plt.title(f"Win rate evolution {distribution}")
     plt.xlabel("Number of action")
@@ -325,8 +399,8 @@ if __name__=="__main__":
     # List of Bandit object 
     # bandits = [Bandit(Bernoulli(p)) for p in Bernoulli_p]
     # or
-    #bandits = [Bandit(Gaussian(mean, stdev)) for (mean, stdev) in Gaussian_params]
-    bandits = [Bandit(Bernoulli(p)) for p in Bernoulli_p]
+    bandits = [Bandit(Gaussian(mean, stdev)) for (mean, stdev) in Gaussian_params]
+    #bandits = [Bandit(Bernoulli(p)) for p in Bernoulli_p]
 
     # Epsilon for the epsilon-greedy algo
     # epsilon = CstEpsilon(EPSILON)
@@ -336,9 +410,11 @@ if __name__=="__main__":
     # Algorithm for the bandit selection
     #algorithm = EpsilonGreedy(bandits, epsilon)
     # or (UCB with c=0.8 for example)
-    #algorithm = UCB(bandits, epsilon, c=0.8)
-    # or
-    algorithm = TS(bandits)
+    #algorithm = UCB(bandits, epsilon, c=0.6)
+    # or for Bernoulli Bandits
+    #algorithm = TS(bandits, Beta)
+    # or for Gaussian Bandits
+    algorithm = TS(bandits, Gaussian)
     # or
     #algorithm = OptimisticInitialValue(bandits, 4)
 
@@ -351,11 +427,11 @@ if __name__=="__main__":
 
     # Number of time each bandit was selected
     for bandit in bandits:
-        print(f"Bandit with E = {bandit.expected_reward()} "
+        print(f"Bandit with \u03B8 = {bandit.expected_reward()} "
               f"was selected {bandit.N} times. "
-              f"Estimated E = {bandit.e:.3f}")
+              f"\u03B8\u0302 = {bandit.e:.3f}")
 
     # In the case that Thompson Sampling is used, it is interesting
-    # to see the Beta distribution of the expected reward
+    # to see the distribution of the expected reward
     algorithm.plot()
     
